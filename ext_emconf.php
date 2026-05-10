43e13c9450d398da17cf56905779042c66566586
Torben Hansen <derhansen@gmail.com>
2026-05-10 19:52:43 +0200

    [TASK] Fixed version constraints in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '9.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.2.0-14.3.99',
+            'typo3' => '14.3.0-14.3.99',
         ],
     ],
 ];
